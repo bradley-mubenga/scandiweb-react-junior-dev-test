4d1ef37826693683b2f0e8a737bac6ac28a2c14f
Bradley Mubenga <mubengabradley@gmail.com>
2021-09-06 13:00:38 +0200

    Attributes Feature Complete

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -8,7 +8,11 @@ export default class ProductPage extends Component {
         super(props);
 
         this.state = {
-            imageIndex: 0
+            imageIndex: 0,
+            attributeSize: 0,
+            attributeColor: 0,
+            attributeCapacity: 0,
+            attributeMisc: 0
         }
     }
 
@@ -31,6 +35,59 @@ export default class ProductPage extends Component {
             imageIndex: imageIndex
         })
     }
+
+    //
+    attributeColor(index) {
+        if (index === this.state.attributeColor) {
+            return ("activeBlockButton")
+        }
+    }
+
+    setColorState(value) {
+        this.setState({
+            attributeColor: value
+        })
+    }
+    
+    //
+    attributeCapacity(index) {
+        if (index === this.state.attributeCapacity) {
+            return ("activeBlockButton")
+        }
+    }
+
+    setCapacityState(value) {
+        this.setState({
+            attributeCapacity: value
+        })
+    }
+    
+    //
+    attributeSize(index) {
+        if (index === this.state.attributeSize) {
+            return ("activeBlockButton")
+        }
+    }
+
+    setSizeState(value) {
+        this.setState({
+            attributeSize: value
+        })
+    }
+
+    //
+    attributeMisc(index) {
+        if (index === this.state.attributeMisc) {
+            return ("activeBlockButton")
+        }
+    }
+
+    setMiscState(value) {
+        this.setState({
+            attributeMisc: value
+        })
+    }
+
     
 
     //
@@ -70,16 +127,20 @@ export default class ProductPage extends Component {
                             </div>
 
                             {
-                                product.attributes.map((attribute, index) => 
-                                    <div className="productSizes" key={index}>
+                                product.attributes.map((attribute, keyIndex) => 
+                                    <div className="productSizes" key={keyIndex}>
                                         <div>
                                             <h3>{attribute.name}</h3>
                                         </div>
                                         <div>
-                                        {
+                                        {   
                                             attribute.items.map((item, index) =>
-                                            <div className="displayValue" key={index}>
-                                                <button className="blockButton" style={(attribute.type === 'swatch') ? ({ backgroundColor: item.value}) : ({})}>{(attribute.type === 'swatch') ? "" : (item.displayValue)}</button>
+                                            <div className={
+                                                (attribute.id === 'Color') ? (this.attributeColor(index)) : (attribute.id === 'Capacity') ? (this.attributeCapacity(index)) : (attribute.id === 'Size') ? (this.attributeSize(index)) : (attribute.id) ? (this.attributeMisc(index)) : ("")
+                                            } key={index}>
+                                                <button className="blockButton" style={(attribute.type === 'swatch') ? ({ backgroundColor: item.value}) : ({})} onClick={
+                                                (attribute.id === 'Color') ? () => this.setColorState(index) : (attribute.id === 'Capacity') ? () => this.setCapacityState(index) : (attribute.id === 'Size') ? () => this.setSizeState(index) : (attribute.id) ? () => this.setMiscState(index) : (() => {return ""})  
+                                                }>{(attribute.type === 'swatch') ? "" : (item.displayValue)}</button>
                                             </div>
                                             )
                                         }
