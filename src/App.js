7c9440cf246b9ae8ca2088360b5b2155a903d9ac
Bradley Mubenga <mubengabradley@gmail.com>
2021-09-09 15:51:13 +0200

    Issue #31 Fixed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //React Components
+import Header from './components/Header';
 import CategoryPage from './pages/CategoryPage';
 import ProductPage from './pages/ProductPage';
 
 export default class App extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+          category: 'all'
+      }
+  }
+
+  switchCategory = (category) => {
+      this.setState({
+          category
+      })
+  }
+
   render() {
     return (
       <Router>
+        <Header switchCategory={this.switchCategory} />
         <Switch>
-        <Route exact path="/">
-            <CategoryPage />
+          <Route exact path="/">
+            <CategoryPage category={this.state.category}/>
           </Route>
-          <Route path="/product/:id" component={ProductPage}/>
+          <Route path="/product/:id" component={ProductPage} />
         </Switch>
       </Router>
     )
